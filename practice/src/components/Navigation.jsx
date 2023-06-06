a6ac277821373e71191105f290ee792ba292e0a2
eunjoo0311 <yyy7423@naver.com>
2023-06-06 19:20:15 +0900

    Feat: navbar 컴포넌트

---
+++ practice/src/components/Navigation.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import homeIcon from "../assets/icon-home.svg";
+import editIcon from "../assets/icon-edit.svg";
+import fillHomeIcon from "../assets/icon-home-fill.svg";
+import messageIcon from "../assets/icon-message-circle.svg";
+import fillMessageIcon from "../assets/icon-message-circle-fill.svg";
+import userIcon from "../assets/icon-user.svg";
+import fillUserIcon from "../assets/icon-user-fill.svg";
+
+const NavWrapper = styled.div`
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const NavLink = styled.a`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+  color: #767676;
+
+  &.active {
+    color: #019223;
+  }
+`;
+
+const NavText = styled.p`
+  margin-top: 4px;
+  font-size: 10px;
+  white-space: nowrap;
+`;
+
+export default function Navigation() {
+  const [activeBtn, setActiveBtn] = useState(null);
+
+  const handleClick = (click, event) => {
+    event.preventDefault();
+    setActiveBtn(click);
+  };
+
+  return (
+    <NavWrapper>
+      <NavLink
+        href="/home"
+        className={activeBtn === "/home" ? "active" : ""}
+        onClick={(event) => handleClick("/home", event)}
+      >
+        <img src={activeBtn === "/home" ? fillHomeIcon : homeIcon} alt="홈" />
+        <NavText>홈</NavText>
+      </NavLink>
+      <NavLink
+        href="/chat"
+        className={activeBtn === "/chat" ? "active" : ""}
+        onClick={(event) => handleClick("/chat", event)}
+      >
+        <img
+          src={activeBtn === "/chat" ? fillMessageIcon : messageIcon}
+          alt="채팅"
+        />
+        <NavText>채팅</NavText>
+      </NavLink>
+      <NavLink
+        href="/post"
+        className={activeBtn === "/post" ? "active" : ""}
+        onClick={(event) => handleClick("/post", event)}
+      >
+        <img src={editIcon} alt="게시물 작성" />
+        <NavText>게시물 작성</NavText>
+      </NavLink>
+      <NavLink
+        href="/profile"
+        className={activeBtn === "/profile" ? "active" : ""}
+        onClick={(event) => handleClick("/profile", event)}
+      >
+        <img
+          src={activeBtn === "/profile" ? fillUserIcon : userIcon}
+          alt="프로필"
+        />
+        <NavText>프로필</NavText>
+      </NavLink>
+    </NavWrapper>
+  );
+}
